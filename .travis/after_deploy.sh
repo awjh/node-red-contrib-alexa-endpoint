e740849be2da84c82977713b33e1b59b8e7e2e78
awjh <andrewhurt@btinternet.com>
2019-07-13 12:29:24 +0100

    version bump changes
    Signed-off-by: awjh <andrewhurt@btinternet.com>

--- .travis/after_deploy.sh
+++ .travis/after_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-npm version patch
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ME=`basename "$0"`
 
+cd $DIR
+
+VERSION=$(npm --no-git-tag-version version patch)
+
+git add $DIR/package.json
+git commit -s -m "Version bump $VERSION"
 git push repo master
